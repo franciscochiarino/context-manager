d20aa106a41d2cd49c4c526fb49a05161f46d90c
fran <francisco.chiarino@gmail.com>
2020-04-22 13:56:49 +0200

    Used database in booksController

--- controllers/booksController.js
+++ controllers/booksController.js
@@ -1,13 +1,32 @@
+const db = require('../models/dataBase');
+
 exports.getBooks = (req, res) => {
-    console.log(req.body)
-    res.send('Get request sent');
+    const books = db.get('books').value();
+    res.json({ success: true, books: books});
 };
+
+exports.getBook = (req, res) => {
+    const id = parseInt(req.params.id);
+    const book = db.get('books').find({id}).value();
+    res.json({ success: true, book: book});
+}
 
 exports.postBook = (req, res) => {
-    res.send('Post request sent');
+    req.body.id = db.get('books').value().length;
+    db.get('books').push(req.body).write();
+    res.json({ success: true, book: db.get('books').last().value()});
 };
 
+exports.putBook = (req, res) => {
+    const id = parseInt(req.params.id);
+    const book = req.body;
+    db.get('books').find({id}).assign(book).write();
+    res.json({ success: true, book: book });
+}
+
 exports.deleteBook = (req, res) => {
-    console.log(req.params.id)
-    res.send('Delete request sent');
+    const id = parseInt(req.params.id);
+    const book = db.get('books').remove({id}).write();
+    res.json({ success: true, book: book });
 };
+
--- routes/booksRoute.js
+++ routes/booksRoute.js
@@ -1,9 +1,11 @@
 const Router = require('express').Router();
-const {getBooks, postBook, deleteBook} = require('../controllers/booksController');
+const {getBooks, getBook, postBook, putBook, deleteBook} = require('../controllers/booksController');
 
 // Books
 Router.get('/', getBooks);
+Router.get('/:id', getBook);
 Router.post('/', postBook);
+Router.put('/:id', putBook);
 Router.delete('/:id', deleteBook);
 
 module.exports = Router;
