f2023455faae7a696b0af58404dbdc83024a7893
fran <francisco.chiarino@gmail.com>
2020-04-24 16:26:13 +0200

    Connected booksController.js functions with mongodb

--- controllers/booksController.js
+++ controllers/booksController.js
@@ -1,25 +1,59 @@
+const Book = require('../models/bookSchema');
 
-exports.getBooks = (req, res) => {
-    res.json({ success: true, books: books});
+exports.getBooks = async (req, res) => {
+    try {
+        const books = await Book.find();
+        res.json({ success: true, books: books});
+    }
+    catch(err) {
+        next(err);
+    }
+    
 };
 
-exports.getBook = (req, res) => {
-
-    res.json({ success: true, book: book});
+exports.getBook = async (req, res) => {
+    const id = req.params.id;
+    try {
+        const book = await Book.findById(id);
+        res.json({ success: true, book: book}); 
+    }
+    catch(err) {
+        next(err);
+    }
+    
 }
 
-exports.postBook = (req, res) => {
-
-    res.json({ success: true, book: book);
+exports.postBook = async (req, res) => {
+    const book = req.body;
+    try {
+        await Book.insertMany(book);
+        res.json({ success: true, book: book}); 
+    }
+    catch(err) {
+        next(err);
+    }
 };
 
-exports.putBook = (req, res) => {
-
-    res.json({ success: true, book: book });
+exports.putBook = async (req, res) => {
+    const id = req.params.id;
+    const book = req.body;
+    try {
+        await Book.findByIdAndUpdate(id, book);
+        res.json({ success: true, book: book}); 
+    }
+    catch(err) {
+        next(err);
+    }
 }
 
-exports.deleteBook = (req, res) => {
-
-    res.json({ success: true, book: book });
+exports.deleteBook = async (req, res) => {
+    const id = req.params.id;
+    try {
+        await Book.findByIdAndDelete(id);
+        res.json({ success: true}); 
+    }
+    catch(err) {
+        next(err);
+    }
 };
 
