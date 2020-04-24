58423258baa312b92750830611518ce046592eaf
fran <francisco.chiarino@gmail.com>
2020-04-24 16:17:38 +0200

    Removed lodb code from booksController.js

--- controllers/booksController.js
+++ controllers/booksController.js
@@ -1,32 +1,25 @@
-const db = require('../models/dataBase');
 
 exports.getBooks = (req, res) => {
-    const books = db.get('books').value();
     res.json({ success: true, books: books});
 };
 
 exports.getBook = (req, res) => {
-    const id = parseInt(req.params.id);
-    const book = db.get('books').find({id}).value();
+
     res.json({ success: true, book: book});
 }
 
 exports.postBook = (req, res) => {
-    req.body.id = db.get('books').value().length;
-    db.get('books').push(req.body).write();
-    res.json({ success: true, book: db.get('books').last().value()});
+
+    res.json({ success: true, book: book);
 };
 
 exports.putBook = (req, res) => {
-    const id = parseInt(req.params.id);
-    const book = req.body;
-    db.get('books').find({id}).assign(book).write();
+
     res.json({ success: true, book: book });
 }
 
 exports.deleteBook = (req, res) => {
-    const id = parseInt(req.params.id);
-    const book = db.get('books').remove({id}).write();
+
     res.json({ success: true, book: book });
 };
 
