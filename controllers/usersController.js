594c1a04c4f3608625c6cd9b7862cd22f24deb2b
fran <francisco.chiarino@gmail.com>
2020-04-24 15:21:20 +0200

    Connected usersController.js functions with mongodb

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,21 +1,57 @@
+const User = require('../models/userSchema');
 
-exports.getUsers = (req, res) => {
-    res.json({ success: true, users: users});
+exports.getUsers = async (req, res) => {
+    try {
+        const users = await User.find();
+        res.json({ success: true, users: users});
+    }
+    catch(err) {
+        next(err);
+    }
 };
 
-exports.getUser = (req, res) => {
-    res.json({ success: true, user: user});
-}
-
-exports.postUser = (req, res) => {
-    res.json({ success: true, user: db.get('users').last().value()});
+exports.getUser = async (req, res) => {
+    const id = req.params.id;
+    try {
+        const user = await User.findById(id);
+        res.json({ success: true, user: user});
+    }
+    catch(err) {
+        next(err);
+    }
 };
 
-exports.putUser = (req, res) => {
-    res.json({ success: true, user: user });
-}
+exports.postUser = async (req, res) => {
+    const user = req.body;
+    try {
+        await User.insertMany(user);
+        res.json({ success: true, user: user});
+    }
+    catch(err) {
+        next(err);
+    }
+};
 
-exports.deleteUser = (req, res) => {
-    res.json({ success: true, user: user });
+exports.putUser = async (req, res) => {
+    const id = req.params.id;
+    const user = req.body;
+    try {
+        await User.findByIdAndUpdate(id, user);
+        res.json({ success: true, user: user });
+    }
+    catch(err) {
+        next(err);
+    }
+};
+
+exports.deleteUser = async (req, res) => {
+    const id = req.params.id;
+    try {
+        await User.findByIdAndDelete(id);
+        res.json({ success: true });
+    }
+    catch(err) {
+        next(err);
+    }
 };
 
