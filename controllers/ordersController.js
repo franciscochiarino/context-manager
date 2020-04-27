de3df979a22603ef743b4c7be4966a79b0c704a8
fran <francisco.chiarino@gmail.com>
2020-04-27 13:48:13 +0200

    Updated ordersController.js

--- controllers/ordersController.js
+++ controllers/ordersController.js
@@ -1,6 +1,9 @@
+const Order = require('../models/orderSchema');
+const createError = require('http-errors');
 
 exports.getOrders = async (req, res) => {
     try {
+        const orders = await Order.find();
         res.json({ success: true, orders: orders});
     }
     catch(err) {
@@ -11,41 +14,44 @@ exports.getOrders = async (req, res) => {
 
 exports.getOrder = async (req, res) => {
     try {
-
+        const order = await Order.find(req.params.id);
+        if (!order) throw createError(404);
+        res.json({ success: true, order: order});
     }
     catch(err) {
         next(err);
     }
-    res.json({ success: true, order: order});
 }
 
 exports.postOrder = async (req, res) => {
     try {
-
+        const order = new Order(req.body);
+        res.json({ success: true, order: order});
     }
     catch(err) {
         next(err);
     }
-    res.json({ success: true, order: db.get('orders').last().value()});
+    
 };
 
 exports.putOrder = async (req, res) => {
     try {
-
+        const updateOrder = await Order.findByIdAndUpdate(req.params.id, req.body, {new: true});
+        res.json({ success: true, order: updateOrder });
     }
     catch(err) {
         next(err);
     }
-    res.json({ success: true, order: order });
 }
 
 exports.deleteOrder = async (req, res) => {
     try {
-
+        const order = await Order.findByIdAndDelete(req.params.id);
+        if (!order) throw createError(404);
+        res.json({ success: true, order: order });
     }
     catch(err) {
         next(err);
     }
-    res.json({ success: true, order: order });
 };
 
