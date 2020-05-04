9331ca34b8aa73b4a0fb078d849418b276302ffa
fran <francisco.chiarino@gmail.com>
2020-05-04 14:28:41 +0200

    Embeded AdressSchema

--- models/userSchema.js
+++ models/userSchema.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const AddressSchema = require('./addressSchema');
 
 const UserSchema = new Schema({
     firstName: {type: String, required: true},
     lastName: {type: String, required: true},
     email: {type: String, required: true},
+    address: AddressSchema,
     password: {type: String, required: true}
 },
 {
