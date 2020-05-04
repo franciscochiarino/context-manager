8c21bfcb636b8ccacdfa52cb8470e310483db1c7
fran <francisco.chiarino@gmail.com>
2020-05-04 14:15:33 +0200

    Validator works fine

--- routes/usersRoute.js
+++ routes/usersRoute.js
@@ -1,9 +1,10 @@
 const Router = require('express').Router();
 const {getUsers, getUser, postUser, putUser, deleteUser} = require('../controllers/usersController');
+const {validateUser} = require('../middlewares/validator');
 
 Router.get('/', getUsers);
 Router.get('/:id', getUser);
-Router.post('/', postUser);
+Router.post('/', validateUser(), postUser);
 Router.put('/:id', putUser);
 Router.delete('/:id', deleteUser);
 
