5856037be2c83823f4f7d1c4d17e3a106578a41f
fran <francisco.chiarino@gmail.com>
2020-05-04 15:04:13 +0200

    Imported function to usersRoute.js

--- routes/usersRoute.js
+++ routes/usersRoute.js
@@ -1,10 +1,11 @@
 const Router = require('express').Router();
-const {getUsers, getUser, postUser, putUser, deleteUser} = require('../controllers/usersController');
+const {getUsers, getUser, postUser, putUser, deleteUser, login} = require('../controllers/usersController');
 const {validateUser} = require('../middlewares/validator');
 
 Router.get('/', getUsers);
 Router.get('/:id', getUser);
 Router.post('/', validateUser(), postUser);
+Router.post('/login', login)
 Router.put('/:id', putUser);
 Router.delete('/:id', deleteUser);
 
